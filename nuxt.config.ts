c73bd0b7d0064ace5d987e70e662b349edb9fdb9
aniket-git-hub <singhdharmvir81@gmail.com>
2023-10-25 16:13:10 +0530

    ADDED: tailwind css

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  css: ['typeface-fira-code']
+  css: ['typeface-fira-code', '~/assets/css/main.css'],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
 })
